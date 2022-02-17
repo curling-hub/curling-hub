feebffe81776ff56338910c2854cb5ec20cfaa78
kotoriのねこ <2567702743@qq.com>
2022-02-17 19:12:57 +0000

    Add example pages to demonstrate oauth2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from "../themes/colors"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
-        </ChakraProvider>
+        <SessionProvider session={pageProps.session} refetchInterval={0} refetchOnWindowFocus={true}>
+            <ChakraProvider theme={theme}>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </SessionProvider>
     )
 }
 
