6484b4eeeb7827c54b0a7181095abd9c6eda22ef
Ross Morrison <ros25@pdx.edu>
2022-02-23 19:26:40 +0000

    centered login box

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import AuthLayout from '../components/layouts/AuthLayout'
+import LoginBox from '../components/LoginBox'
 
 const Login: NextPage = () => {
     return (
@@ -9,11 +10,13 @@ const Login: NextPage = () => {
                 <title>Login | Curlo</title>
             </Head>
             <div style={{
-                background: "linear-gradient(#735FED, #FFFFFF)",
+                background: 'linear-gradient(#735FED, #FFFFFF)',
                 height: "100vh",
                 width: '100%'
             }}>
-                <AuthLayout />
+                <AuthLayout>
+                    <LoginBox email="email" password="password"/>
+                </AuthLayout>
             </div>
         </>
     )
