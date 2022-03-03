d453bc3fce05ccf582f34dff594f1bf30868d0ab
Vincent Vermilya <TapeWerm@users.noreply.github.com>
2022-03-03 02:57:34 +0000

    Fix parenthesis

--- cypress/integration/about.spec.js
+++ cypress/integration/about.spec.js
@@ -1,8 +1,8 @@
-describe('About page rendering'), () => {
+describe('About page rendering', () => {
     const port = Cypress.env('PORT') || '3000'
     const baseUrl = `http://localhost:${port}`
-    it('should contain about'), () => {
+    it('should contain about', () => {
         cy.visit(`${baseUrl}/about`)
         cy.get('p').contains('Curlo is a not for profit website')
-    }
-}
+    })
+})
