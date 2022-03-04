e091fc397948e69fd9869cd5665f11b17605a475
RayChen29 <82558712+RayChen29@users.noreply.github.com>
2022-03-04 15:24:24 -0800

    Left Card's Width adjusted to match Right.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import StandardLayout from "../components/layouts/StandardLayout";
-import { Box } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
-import { Text } from "@chakra-ui/react";
-import { Flex, Spacer } from "@chakra-ui/react";
-import { Center } from "@chakra-ui/react";
-import { Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Text,
+  Flex,
+  Spacer,
+  Center,
+  Link,
+} from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
@@ -24,98 +27,108 @@ const Home: NextPage = () => {
         <StandardLayout />
         {/*Card Container Box*/}
         <Flex
-          display="wrap"
           alignItems="center"
-          marginLeft={{ base: "0", md: "12.5%", lg: "25%" }}
-          justify="center"
+          //marginLeft={{base:"0",md:"12.5%",lg:"25%"}}
+          //        justify ="center"
+          direction={{ base: "column", md: "row" }}
         >
-          <Box
-            display="inline-block"
-            background="#FFFFFF"
-            alignItems="center"
-            padding="1rem"
-            borderRadius="35px"
-            marginTop={"7%"}
-            textAlign="center"
-            boxShadow="lg"
-          >
-            <Text fontSize="2.5rem" marginTop="5px">
-              <b>Get Started</b>
-            </Text>
-            <Text fontSize="1.4rem">
-              Want to join a local <br></br>
-              curling club? Join <br></br>today!
-            </Text>
-            <br></br>
-            <span>
+          <Spacer />
+          <Center>
+            <Box
+              display="inline-block"
+              background="primary.white"
+              alignItems="center"
+              padding="1rem"
+              borderRadius="35px"
+              marginTop={"7%"}
+              textAlign="center"
+              boxShadow="lg"
+              //     marginBottom={{md:'50px' }}>
+            >
+              <Text fontSize="2.5rem" marginTop="5px" fontWeight="bold">
+                Get Started
+              </Text>
+
+              <Text fontSize="1.4rem" marginX="30px">
+                Want to join a local <br></br>
+                curling club? Join <br></br>today!
+              </Text>
+
               <Link href="/signup">
                 <Button
-                  background="#7FD6A4"
+                  background="primary.green"
                   borderRadius="35px"
-                  marginTop={{ base: "0", md: "17.5vh", lg: "35vh" }}
-                  padding="25px"
+                  //           marginTop={{base:"0",md:"17.5vh",lg:"35vh"}}
+                  padding="25px 37px"
                   fontSize="19px"
+                  boxShadow="md"
+                  marginTop="37.7vh"
                 >
-                  <b>Sign Up</b>
+                  Sign Up
                 </Button>
               </Link>
-            </span>
-            <span>
+
               <Link href="/about">
                 <Button
-                  background="#7FD6A4"
+                  background="primary.green"
                   borderRadius="35px"
                   marginLeft="15px"
-                  marginTop={{ base: "0", md: "17.5vh", lg: "35vh" }}
-                  padding="25px"
+                  //            marginTop={{base:"0",md:"17.5vh",lg:"35vh"}}
+                  marginTop="37.7vh"
+                  padding="25px 37px"
                   fontSize="19px"
+                  boxShadow="md"
                 >
-                  <b>More Info </b>
+                  More Info
                 </Button>
               </Link>
-            </span>
-          </Box>{" "}
-          {/*Card1}*/}
-          {/*Card 2 */}
-          <Box
-            display="inline-block"
-            background="#FFFFFF"
-            alignItems="center"
-            padding="0.9rem 0.9rem 0.9rem 0.9rem"
-            borderRadius="35px"
-            marginTop={"7%"}
-            //          marginLeft="25%"
-            marginLeft={{ base: "0%", md: "0%", lg: "25%" }}
-            //   marginRight={{base:"0",md:"5%",lg:"10%"}}
-            textAlign="center"
-            boxShadow="lg"
-          >
-            <Text fontSize="2.5rem" marginTop="5px">
-              <b>Ratings</b>
-            </Text>
-            <Text fontSize="1.4rem">
-              Want to see the top teams <br></br>
-              in the world? Check out <br></br>
-              the ratings below.
-            </Text>
-            <br></br>
+            </Box>{" "}
+            {/*Card1}*/}
+          </Center>
+          <Spacer />
+          <Center>
+            {/*Card 2 */}
+            <Box
+              display="inline-block"
+              background="primary.white"
+              alignItems="center"
+              padding="0.9rem 0.9rem 0.9rem 0.9rem"
+              borderRadius="35px"
+              marginTop={"7%"}
+              boxShadow="lg"
+              //             marginLeft={{base:"0%",md:"0%",lg:"25%"}}
 
-            <span>
-              <Link href="/ratings">
-                <Button
-                  background="#7FD6A4"
-                  borderRadius="full"
-                  marginLeft="15px"
-                  marginTop={{ base: "0", md: "17.5vh", lg: "35vh" }}
-                  padding="25px 99px"
-                  fontSize="19px"
-                >
-                  <b>Ratings</b>
-                </Button>
-              </Link>
-            </span>
-          </Box>{" "}
-          {/*Card2}*/}
+              textAlign="center"
+            >
+              <Text fontSize="2.5rem" marginTop="5px" fontWeight="bold">
+                Ratings
+              </Text>
+              <Text fontSize="1.4rem" marginX="30px">
+                Want to see the top teams <br></br>
+                in the world? Check out <br></br>
+                the ratings below.
+              </Text>
+              <br></br>
+
+              <span>
+                <Link href="/ratings">
+                  <Button
+                    background="#7FD6A4"
+                    borderRadius="full"
+                    marginLeft="15px"
+                    //            marginTop={{base:"0",md:"17.5vh",lg:"35vh"}}
+                    marginTop="35vh"
+                    padding="25px 99px"
+                    fontSize="19px"
+                  >
+                    <b>Ratings</b>
+                  </Button>
+                </Link>
+              </span>
+            </Box>{" "}
+            {/*Card2}*/}
+          </Center>
+          <Spacer />
         </Flex>{" "}
         {/*Card Container*/}
       </Box>
