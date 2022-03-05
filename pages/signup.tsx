6fbf7d9917eb94bb0e461a75d61cb024c1b27eb7
BennettDesmond <bennett.a.desmond@gmail.com>
2022-03-05 19:54:56 +0000

    fixing build errors

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -31,19 +31,10 @@ import TermsOfServiceModal from '../components/modals/TermsOfServiceModal'
 import PrivacyPolicyModal from '../components/modals/PrivacyPolicyModal'
 
 
-
-enum MainField {
-    SIGNUP,
-    PRIVACY_POLICY,
-    TERMS_OF_SERVICE,
-}
-
-
 const Signup: NextPage = () => {
     const [email, setEmail] = useState("")
     const [isHost, setIsHost] = useState(false)
     const [mounted, setMounted] = useState(false)
-    const [mainField, setMainField] = useState<MainField>(MainField.SIGNUP)
     const hostAccountDisclosure = useDisclosure()
     const {
         isOpen: privacyPolicyIsOpen = false,
@@ -73,9 +64,9 @@ const Signup: NextPage = () => {
                 bgGradient="linear-gradient(#735FED, #FFFFFF) repeat"
             >
                 <AuthLayout>
-                    <Container maxW="2xl">
+                    <Container maxW="2xl" centerContent>
                         {/* Outer box */}
-                        <Box minW="sm" w="100%" h={signupContainerHeight} my="4" borderRadius="20" bg="white" shadow="md">
+                        <Box minW="sm" maxW={{ base: "sm", md: "none" }} w="100%" h={signupContainerHeight} my="4" borderRadius="20" bg="white" shadow="md">
                             <Flex flexDirection="row" h="100%">
                                 {/* Left */}
                                 <Box
@@ -87,20 +78,16 @@ const Signup: NextPage = () => {
                                 >
                                 </Box>
                                 {/* Sign up container (should only run on client side, e.g mounted) */}
-                                <Box minW="sm" w="100%" h="100%" m={2} p={10} borderRadius="32">
+                                <Box minW="sm" w="100%" h="100%" m={{ base: 0, md: 2 }} p={10} borderRadius="32">
                                     {mounted && (
                                         <>
-                                            <PrivacyPolicyPopover
+                                            <PrivacyPolicyModal
                                                 isOpen={privacyPolicyIsOpen}
-                                                onOpen={privacyPolicyOnOpen}
                                                 onClose={privacyPolicyOnClose}
-                                                h={popoverHeight}
                                             />
-                                            <TermsOfServicePopover
+                                            <TermsOfServiceModal
                                                 isOpen={termsOfServiceIsOpen}
-                                                onOpen={termsOfServiceOnOpen}
                                                 onClose={termsOfServiceOnClose}
-                                                h={popoverHeight}
                                             />
                                             <HostAccountPopover
                                                 isOpen={hostAccountDisclosure.isOpen}
@@ -150,7 +137,7 @@ interface SignupFieldsProps {
     onEmailChange: (email: string) => void;
     isHost: boolean;
     onIsHostChange: () => void;
-    hostAccountDisclosure?: ReturnType<typeof useDisclosure>;
+    hostAccountDisclosure: ReturnType<typeof useDisclosure>;
     onOpenPrivacyPolicy: () => void;
     onOpenTermsOfService: () => void;
 }
@@ -165,8 +152,7 @@ function SignupFields(props: SignupFieldsProps) {
         onOpenTermsOfService,
         hostAccountDisclosure,
     } = props
-    const haDisclosure = hostAccountDisclosure || useDisclosure()
-    const { onOpen: onPopoverOpen, onClose: onPopoverClose, isOpen: isPopoverOpen } = haDisclosure
+    const { onOpen: onPopoverOpen, onClose: onPopoverClose, isOpen: isPopoverOpen } = hostAccountDisclosure
     const helperTextFontSize = "12"
     return (
         <VStack alignItems="start" spacing="4">
@@ -274,127 +260,6 @@ function HostAccountPopover(props: HostAccountPopoverProps) {
                 </PopoverBody>
             </PopoverContent>
         </Popover>
-    )
-}
-
-interface TermsOfServicePopoverProps {
-    h?: number | string
-    isOpen?: boolean
-    onOpen?: () => void
-    onClose?: () => void
-}
-
-function TermsOfServicePopover(props: TermsOfServicePopoverProps) {
-    const { h, isOpen = false, onOpen = (() => { }), onClose = (() => { }) } = props
-    return (
-        <Popover
-            isOpen={isOpen}
-            onOpen={onOpen}
-            onClose={onClose}
-        >
-            <PopoverAnchor>
-                <Text>{" "}</Text>
-            </PopoverAnchor>
-            <PopoverContent w="sm" borderRadius={16} h={h} border="none" bg="primary.green">
-                <PopoverHeader>
-                    Terms of Service
-                </PopoverHeader>
-                <PopoverCloseButton />
-                <PopoverBody>
-                    <TermsOfService />
-                </PopoverBody>
-            </PopoverContent>
-        </Popover>
-    )
-}
-
-function TermsOfService() {
-    // TODO: Placeholder
-    return (
-        <Box overflowY="scroll" maxH="240" textColor="black" fontSize="12">
-            <Text>
-                Terms of service placeholder
-            </Text>
-        </Box>
-    )
-}
-
-interface PrivacyPolicyPopoverProps {
-    h?: number | string
-    isOpen?: boolean
-    onOpen?: () => void
-    onClose?: () => void
-}
-
-function PrivacyPolicyPopover(props: PrivacyPolicyPopoverProps) {
-    const { h, isOpen = false, onOpen = (() => { }), onClose = (() => { }) } = props
-    return (
-        <Popover
-            isOpen={isOpen}
-            onOpen={onOpen}
-            onClose={onClose}
-        >
-            <PopoverAnchor>
-                <Text>{" "}</Text>
-            </PopoverAnchor>
-            <PopoverContent w="sm" maxW="sm" borderRadius={16} h={h} border="none" bg="primary.green">
-                <PopoverHeader>
-                    Privacy Policy
-                </PopoverHeader>
-                <PopoverCloseButton />
-                <PopoverBody>
-                    <PrivacyPolicy />
-                </PopoverBody>
-            </PopoverContent>
-        </Popover>
-    )
-}
-
-function PrivacyPolicy() {
-    // TODO: Placeholder
-    return (
-        <Box overflowY="scroll" maxH="240" textColor="black" fontSize="12">
-            <Text>
-                We value your privacy and strive to protect your personal information.
-                Please read this Policy to understand what types of information we collect from you,
-                for what purposes and what choices you have regarding our collection of your information.
-            </Text>
-            <Text>
-                This policy covers the Curlo website. By accessing, using or posting information
-                to this Website, you agree to this Privacy Policy.
-            </Text>
-            <Text>
-                We collect several types of information from and about you, including:
-            </Text>
-            <Text>
-                1. Your email address and password. We treat this information as "Personally
-                Identifiable Information" or "PII". We never store passwords in plain text format,
-                only secure password hashes.
-            </Text>
-            <Text>
-                2. Non-personally identifiable information, such as demographic information about you,
-                information about your computer system or device, your preferences, your online activity,
-                and your location information ("Non-Personally Identifiable Information" a "Non-PII").
-                Non-PII, by itself, does not identify you, but it can be combined with other information
-                in a way that allows you to be identified. If this happens, we will treat the combined
-                information as PII.
-            </Text>
-            <Text>
-                3. Specific to users of Curlo, we (optionally) collect personal information such as your gender,
-                data of birth, and other information required for computation, analysis and display of Curlo ratings.
-            </Text>
-            <Text>
-                We may collect information from or about you in the following ways:
-            </Text>
-            <Text>
-                Information Provided by You. We collect information provided by you when you create
-                your profile or use Curlo services.
-            </Text>
-            <Text>
-                We do not sell, rent, or lease any of your personal information to third parties
-                without your explicit consent.
-            </Text>
-        </Box>
     )
 }
 
