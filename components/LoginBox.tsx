b547b5fe63e9b2d00635412862283868dc463daa
andresh6 <43798349+andresh6@users.noreply.github.com>
2022-03-09 19:04:14 -0800

    made login page responsive and added tos and pp modals (#18)
    Co-authored-by: Andres <herrera8@capstone-dev-0d66.us-west1-b.c.capstone-340900.internal>

--- components/LoginBox.tsx
+++ components/LoginBox.tsx
@@ -1,120 +1,102 @@
-import { Box, BoxProps } from "@chakra-ui/react"
-import { Button } from "@chakra-ui/react"
-import { Divider } from "@chakra-ui/react"
-import { Input } from "@chakra-ui/react"
-import { InputGroup } from "@chakra-ui/react"
-import { Text } from "@chakra-ui/react"
-import { getCsrfToken } from "next-auth/react"
-import { FcGoogle } from 'react-icons/fc'
-import {useState, ChangeEvent} from 'react'
-import { setValues } from "framer-motion/types/render/utils/setters"
+import NextLink from 'next/link'
+import { signIn } from 'next-auth/react'
+import { FaGoogle } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import {
+    Button,
+    Checkbox,
+    Divider,
+    FormControl,
+    HStack,
+    Link as ChakraLink,
+    Input,
+    Text,
+    Stack,
+    VStack,
+    Select,
+    Center
+} from '@chakra-ui/react'
 
-export default function LoginBox() {
-    const [email, setEmail] = useState('')
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)
-    const loginRegular = () => {console.log(email)}
-    const loginGoogle = () => {console.log(email + " with google")}
+interface LoginFieldsProps {
+    email: string;
+    onEmailChange: (email: string) => void;
+    onOpenPrivacyPolicy: () => void;
+    onOpenTermsOfService: () => void;
+}
 
+export default function LoginFields(props: LoginFieldsProps) {
+    const {
+        email,
+        onEmailChange,
+        onOpenPrivacyPolicy,
+        onOpenTermsOfService,
+    } = props
+
+    const helperTextFontSize = "12"
     return (
-        <Box
-            width='50%'
-            height='60%'
-            borderRadius='25px'
-            backgroundColor={'white'}
-        >
-            <Box
-                 width='70%'
-                 height='100%'
-                 backgroundColor={'#7fd6a4'}
-                 borderRadius='25px'
-                 display="flex"
-                 flexDirection="column"
-                 alignItems="center"
-                 justifyContent='center'
-                 overflow='auto'
+
+        <VStack alignItems="start" spacing="4" verticalAlign="center">
+
+            <Text fontSize='3xl' text-align="center">
+                Log In
+            </Text>
+            <FormControl>
+                <Stack>
+                    <Input
+                        borderRadius="full"
+                        focusBorderColor="green.400"
+                        shadow="sm"
+                        placeholder="Email Address"
+                        value={email}
+                        onChange={(e) => onEmailChange(e.target.value)}
+                    />
+                </Stack>
+            </FormControl>
+            <Button
+                leftIcon={<HiOutlineMail />}
+                isFullWidth
+                bg="primary.gray"
+                borderRadius="full"
+                boxShadow="md"
+                _hover={{ bg: "gray.400" }}
+                _active={{ bg: "gray.600" }}
+                _focus={{ boxShadow: "lg" }}
+                onClick={() => signIn("email", { email })}//TODO this needs to be changed
             >
-                <Box
-                    display="flex"
-                    flexDirection="column"
-                    alignItems="left"
-                    justifyContent='center'
-                    minWidth='60%'
-                    height='100%'
-                    boxShadow={1}
-                    rowGap='5%'
-                    py={4}
-                    px={{ base: 4, md: 8 }}
-                >
-                    <Text fontSize="4xl">
-                        Log In
-                    </Text>
-                    <InputGroup display="flex" flexDirection="column">
-                        <Input
-                            variant="outline"
-                            colorScheme="white"
-                            borderColor={'#C4C4C4'}
-                            mt={2}
-                            isRequired={true}
-                            fontSize="sm"
-                            type="email"
-                            placeholder="email address"
-                            _placeholder={{color: 'black'}}
-                            errorBorderColor="red.500"
-                            onChange={handleChange}
-                        />
-                    </InputGroup>
-                    <Button
-                        mt={2}
-                        width="100%"
-                        height="9%"
-                        color='#C4C4C4'
-                        textColor={"#000000"}
-                        onClick={loginRegular}
-                        overflow='clip'
-                        display='block'
-                        textOverflow='ellipsis'
-                    >
-                        Login with email
+                Log in with Email
+            </Button>
+            <Button
+                leftIcon={<FaGoogle />}
+                isFullWidth
+                bg="primary.gray"
+                borderRadius="full"
+                boxShadow="md"
+                _hover={{ bg: "gray.400" }}
+                _active={{ bg: "gray.600" }}
+                _focus={{ boxShadow: "lg" }}
+                onClick={() => signIn("email", { email })}//TODO this needs to be changed
+            >
+                Log in with Google
+            </Button>
+
+            <VStack w="100%" spacing="1">
+
+                <Text fontSize={helperTextFontSize}>
+                    Don't have an account?{" "}
+                    <NextLink href="/newuser" passHref>
+                        <ChakraLink textColor="primary.black" >Sign Up</ChakraLink>
+                    </NextLink>
+                </Text>
+                <Text fontSize={helperTextFontSize}>
+                    <Button textColor="primary.black" variant="link" size={helperTextFontSize} onClick={onOpenTermsOfService}>
+                        Terms of Service
                     </Button>
-                    <Button
-                        mt={2}
-                        width="100%"
-                        height="9%"
-                        color='#C4C4C4'
-                        textColor={"#000000"}
-                        rightIcon={<FcGoogle/>}
-                        onClick={loginGoogle}
-                        overflow='clip'
-                        display='block'
-                        textOverflow='ellipsis'              >
-                        Login with Google
+                    {" "}and{" "}
+                    <Button textColor="primary.black" variant="link" size={helperTextFontSize} onClick={onOpenPrivacyPolicy}>
+                        Privacy Policy
                     </Button>
-                    <Text>
-                        {"Don't have an account? "}
-                        <Button
-                            variant='link'
-                            color='black'
-                        >
-                            Sign Up
-                        </Button>
-                    </Text>
-                    <Text>
-                        <Button
-                            variant='link'
-                            color='black'
-                        >
-                            Terms of service
-                        </Button>
-                        {" and "}
-                        <Button
-                            variant='link'
-                            color='black'
-                        >
-                            Privacy Policy
-                        </Button>
-                    </Text>
-                </Box>
-            </Box>
-        </Box>
+                </Text>
+            </VStack>
+        </VStack>
     )
 }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,25 +1,102 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
+import { getSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+// import React, { Component, Fragment } from 'react'
+import TermsOfServiceModal from '../components/modals/TermsOfServiceModal'
+import PrivacyPolicyModal from '../components/modals/PrivacyPolicyModal'
 import AuthLayout from '../components/layouts/AuthLayout'
-import LoginBox from '../components/LoginBox'
+import NewHostFields from '../components/newHost/newHostFields'
+import LoginFields from '../components/LoginBox'
+import {
+    Box,
+    Container,
+    Flex,
+    useDisclosure,
+} from '@chakra-ui/react'
 
-const Login: NextPage = () => {
+
+const NewHost: NextPage = () => {
+    const [email, setEmail] = useState("")
+    const [mounted, setMounted] = useState(false)
+
+    const {
+        isOpen: privacyPolicyIsOpen = false,
+        onOpen: privacyPolicyOnOpen,
+        onClose: privacyPolicyOnClose,
+    } = useDisclosure()
+    const {
+        isOpen: termsOfServiceIsOpen = false,
+        onOpen: termsOfServiceOnOpen,
+        onClose: termsOfServiceOnClose,
+    } = useDisclosure()
+    const signupContainerHeight = "330"
+    const popoverHeight = "450"
+
+    useEffect(() => { setMounted(true) }, [])
+
     return (
         <>
             <Head>
                 <title>Login | Curlo</title>
             </Head>
-            <div style={{
-                background: 'linear-gradient(#735FED, #FFFFFF)',
-                height: "100vh",
-                width: '100%'
-            }}>
+            <Box
+                position="absolute"
+                w="100%"
+                h="100vh"
+                minW="md"
+                bgGradient="linear-gradient(#735FED, #FFFFFF) repeat"
+            >
                 <AuthLayout>
-                    <LoginBox/>
+                    <Container maxW="xl" centerContent>
+                        {/* Outer box */}
+                        <Box minW="sm" maxW={{ base: "sm", md: "none" }} w="100%" h={signupContainerHeight} my="4" borderRadius="20" bg="white" shadow="md">
+                            <Flex flexDirection="row" h="100%">
+                                <Box minW="sm" w="50%" h="100%" m={{ base: 0, md: 0 }} p={10} borderRadius="20" bg="primary.green" >
+                                    {mounted && (
+                                        <>
+                                            <PrivacyPolicyModal
+                                                isOpen={privacyPolicyIsOpen}
+                                                onClose={privacyPolicyOnClose}
+                                            />
+                                            <TermsOfServiceModal
+                                                isOpen={termsOfServiceIsOpen}
+                                                onClose={termsOfServiceOnClose}
+                                            />
+                                            <LoginFields
+                                                email={email}
+                                                onEmailChange={setEmail}
+                                                onOpenPrivacyPolicy={() => { privacyPolicyOnOpen() }}
+                                                onOpenTermsOfService={() => { termsOfServiceOnOpen() }}
+                                            />
+                                        </>
+                                    )}
+
+                                </Box>
+                            </Flex>
+                        </Box>
+                    </Container>
                 </AuthLayout>
-            </div>
+            </Box>
         </>
     )
 }
 
-export default Login
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const session = await getSession(context)
+    if (!session || !session["user"]) {
+        // not signed in
+        return {
+            props: {}
+        }
+    }
+    // already signed in, redirect
+    return {
+        // redirect: {
+        //     destination: "/",
+        // },
+        props: {},
+    }
+}
+
+export default NewHost 
