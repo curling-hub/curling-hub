201f10fbbd04442066a61ddc8e407e312a1736ed
Bennett Desmond <bennett.a.desmond@gmail.com>
2022-03-10 17:24:58 -0800

    Landing page3.0 (#20)
    * Add signup page with placeholder privacy policy and TOS
    
    * Add Popover implementation
    
    * worked on the modal
    
    * Created cards, text, and buttons.
    
    * Added Responsiveness to cards' spacing and buttons
    
    * Just enabling Cypress
    
    * Added tests
    
    * Cards should have equal spacing for the most part
    
    * Left Card's Width adjusted to match Right.
    
    * Removed Line 1 As Per Request.
    
    * Cards' dimensions adjusted to match
    
    * merge conflicts resolved
    
    * adjusted the landing page file and added some defined constants
    
    * rebase
    
    * fixing build errors
    
    * Add signup page with placeholder privacy policy and TOS
    
    * Add Popover implementation
    
    * rebase
    
    * Created cards, text, and buttons.
    
    * Added Responsiveness to cards' spacing and buttons
    
    * Just enabling Cypress
    
    * Added tests
    
    * Cards should have equal spacing for the most part
    
    * Left Card's Width adjusted to match Right.
    
    * Removed Line 1 As Per Request.
    
    * Cards' dimensions adjusted to match
    
    * adjusted the landing page file and added some defined constants
    
    * rebase
    
    * Fixing formatting issues (No code changes)
    
    Co-authored-by: kotoriのねこ <2567702743@qq.com>
    Co-authored-by: RayChen29 <82558712+RayChen29@users.noreply.github.com>
    Co-authored-by: RaymondChen29 <rc29@pdx.edu>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import StandardLayout from '../components/layouts/StandardLayout'
+import type { NextPage } from "next";
+import Head from "next/head";
+import { REG_BUTTON_FONT_SIZE, CONST_BORDER_RADIUS } from "../themes/constants";
+import StandardLayout from "../components/layouts/StandardLayout";
+import { Box, Button, Text, Flex, Spacer, Link } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
@@ -8,15 +10,110 @@ const Home: NextPage = () => {
       <Head>
         <title>Home | Curlo</title>
       </Head>
-      <div style={{
-        background: "linear-gradient(#735FED, #FFFFFF)",
-        height: "100vh",
-        width: '100%'
-      }}>
+      <Box
+        position="absolute"
+        w="100%"
+        h="100vh"
+        bgGradient="linear-gradient(primary.purple, primary.white)"
+      >
         <StandardLayout />
-      </div>
-    </>
-  )
-}
+        {/*Card Container Box*/}
+        <Flex alignItems="center" direction={{ base: "column", md: "row" }}>
+          <Spacer />
+          <Box
+            background="primary.white"
+            alignItems="center"
+            padding="1rem"
+            borderRadius={CONST_BORDER_RADIUS}
+            marginTop="2%"
+            marginBottom="50px"
+            textAlign="center"
+            boxShadow="lg"
+            width="320px"
+            height="550px"
+          >
+            <Text fontSize="2.5rem" marginTop="5px" fontWeight="bold">
+              Get Started
+            </Text>
 
-export default Home
+            <Text fontSize="1.3rem" marginX="20px">
+              Want to join a local
+              <br />
+              curling club?
+              <br />
+              Join today!
+            </Text>
+
+            <Box marginTop="308px">
+              <Link href="/signup">
+                <Button
+                  background="primary.green"
+                  borderRadius="full"
+                  padding="25px 30px"
+                  fontSize={REG_BUTTON_FONT_SIZE}
+                  boxShadow="md"
+                  marginRight="10px"
+                >
+                  Sign Up
+                </Button>
+              </Link>
+              <Link href="/about">
+                <Button
+                  background="primary.green"
+                  borderRadius="full"
+                  padding="25px 30px"
+                  fontSize={REG_BUTTON_FONT_SIZE}
+                  boxShadow="md"
+                >
+                  More Info
+                </Button>
+              </Link>
+            </Box>
+          </Box>{" "}
+          {/*Card1}*/}
+          <Spacer />
+          {/*Card 2 */}
+          <Box
+            background="primary.white"
+            alignItems="center"
+            padding="0.9rem 0.9rem 0.9rem 0.9rem"
+            borderRadius={CONST_BORDER_RADIUS}
+            marginTop="2%"
+            marginBottom="50px"
+            boxShadow="lg"
+            width="320px"
+            height="550px"
+            textAlign="center"
+          >
+            <Text fontSize="2.5rem" marginTop="5px" fontWeight="bold">
+              Ratings
+            </Text>
+            <Text fontSize="1.3rem" marginX="20px">
+              Want to see the top teams <br></br>
+              in the world? Check out <br></br>
+              the ratings below.
+            </Text>
+            <Box marginTop="308px">
+              <Link href="/ratings">
+                <Button
+                  background="primary.green"
+                  borderRadius="full"
+                  boxShadow="md"
+                  padding="25px 108px"
+                  fontSize={REG_BUTTON_FONT_SIZE}
+                >
+                  Ratings
+                </Button>
+              </Link>
+            </Box>
+          </Box>{" "}
+          {/*Card2}*/}
+          <Spacer />
+        </Flex>{" "}
+        {/*Card Container*/}
+      </Box>
+    </>
+  );
+};
+
+export default Home;
---
+++ themes/constants.ts
@@ -0,0 +1,2 @@
+export const REG_BUTTON_FONT_SIZE = '19px'
+export const CONST_BORDER_RADIUS = '35px'
