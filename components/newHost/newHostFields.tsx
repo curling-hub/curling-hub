520c339ed61599c4e907d76ab46b577c1f665382
Ross Morrison <ros25@pdx.edu>
2022-03-15 01:25:59 +0000

    fixed phone RE

--- components/newHost/newHostFields.tsx
+++ components/newHost/newHostFields.tsx
@@ -79,7 +79,7 @@ export default function NewHostFields(props: NewHostFieldsProps) {
         onOpenTermsOfService
     } = props
 
-    const phoneRE = /^\D[0-9]{3}[\W]*[0-9]{3}[\W]*[0-9]{4}/;
+    const phoneRE = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/g;
     let hostSignupSchema = object({
         organization: string().required(), // TODO: ??
         website: string().required().url().nullable(),
