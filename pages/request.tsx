b567cdec06681e4c4df8167959a97c7fd83c21b7
BennettDesmond <bennett.a.desmond@gmail.com>
2022-03-15 21:01:40 +0000

    adjusted spacing and modal component

--- pages/request.tsx
+++ pages/request.tsx
@@ -1,20 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import AuthLayout from '../components/layouts/AuthLayout'
+import RequestModal from '../components/modals/RequestModal'
 import {
-    Box, Text, Image, Button, Spacer, Center, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton
+    Box, Text, Image, Button, Spacer, Center, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, VStack
 } from '@chakra-ui/react'
 import React, { useState } from 'react'
+const linkTextSize = "12"
 
 
 
 const Request: NextPage = () => {
-    const{isOpen,onOpen,onClose} = useDisclosure()
+    const { isOpen, onOpen, onClose } = useDisclosure()
     const [size, setSize] = useState('sm')
-   
+
     const handleSizeClick = (newSize: React.SetStateAction<string>) => {//You can ignore the newSize error/warning, for now?
-      setSize(newSize)
-      onOpen()
+        setSize(newSize)
+        onOpen()
     }
 
     const sizes = ['xl']
@@ -32,93 +34,108 @@ const Request: NextPage = () => {
             >
                 <AuthLayout>
                     <Box
-                 //   alignItems='center'
-                    justifyItems='center'
-                    minWidth="29.3%"
-                    minHeight="60%"
-                    background="primary.green"
-                    borderRadius="35px"
-                    marginBottom="10%"
-                //    marginLeft='0%'
+                        alignItems='center'
+                        // justifyItems='center'
+                        // minWidth="29.3%"
+                        width={{ base: "300px", md: "400px", lg: "400px" }}
+                        // minHeight="60%"
+                        height={{ base: "300px", md: "400px", lg: "400px" }}
+                        background="primary.green"
+                        borderRadius="35px"
+                        marginBottom={{ base: '50%', md: '30%', lg: '28%' }}
+                    //    marginLeft='0%'
                     >
-                        <Text
-                        fontSize="1.9vw"
-                        textAlign='center'
-                        fontWeight='bold'
-                        padding="7% 7% 0% 7%">
-                            We are processing your <br></br>
-                            request. Expect to hear <br></br>
-                            from us as we verify <br></br>
-                            your account.
-                        </Text>
+                        <VStack spacing="3" marginTop="8%">
+                            <Text
+                                fontSize="20px"
+                                textAlign='center'
+                                fontWeight='bold'
+                                padding="6% 6% 0% 6%">
+                                We are processing your <br></br>
+                                request. Expect to hear <br></br>
+                                from us as we verify <br></br>
+                                your account.
+                            </Text>
 
-                        <Image src="/curloLogo2.svg" 
-                                        alt="Curlo logo" 
-                                        w="6vw" 
-                                        h="6vw" 
-                                        marginLeft='40%'/>
-                                    {/*//Image End */}     
+                            <Image src="/curloLogo2.svg"
+                                alt="Curlo logo"
+                                // w="6vw"
+                                // h="6vw"
+                                width={{ base: '70px', md: '100px', lg: '120x' }}
+                                height={{ base: '70px', md: '100px', lg: '120px' }}
+                                marginLeft='40%' />
+                            {/*//Image End */}
 
-                        {sizes.map((size) => (
-                            <Button 
-                                backgroundColor='primary.green'
-                                h='80px'
-                                justifySelf={'center'}
+                            {/* {sizes.map((size) => (
+                                <Button
+                                    backgroundColor='primary.green'
+                                    h='80px'
+                                    justifySelf={'center'}
 
-                                onClick={() => handleSizeClick(size)}
-                                key={size}
-                                alignContent='center'
-                                marginLeft={{base:'0%',lg:'2%', xl:'27%'}}
-                                marginBottom='15%'
-                                
-                              //  marginLeft='17%'
+                                    onClick={() => handleSizeClick(size)}
+                                    key={size}
+                                    alignContent='center'
+                                    marginLeft={{ base: '0%', lg: '2%', xl: '27%' }}
+                                    marginBottom='15%'
+
+                                //  marginLeft='17%'
                                 //marginBottom = essentially adds padding to the bottom, rather than bring text+button closer to logo.
                                 >
-       
-                                    <Text
-                                        fontSize={{base:'1em', md:'1.1em',lg:'1.2em',xl:'1.4em'}}
-//                                        fontSize='2em'
+
+                                    {/* <Text
+                                        fontSize={{ base: '1em', md: '1.1em', lg: '1.2em', xl: '1.4em' }}
+                                    //                                        fontSize='2em'
                                     >
                                         What is account verification?
-                                    </Text>
-                            </Button>   
-                        ))}
-                        <Modal 
+                                    </Text> */}
+                            {/* </Button>
+                            ))} */}
+                            <Text fontSize={linkTextSize}>
+                                <Button variant="link" size={linkTextSize} onClick={onOpen}>
+                                    What is account verification?
+                                </Button>
+                            </Text>
+                            <RequestModal
+                                isOpen={isOpen}
+                                onClose={onClose}
+                            />
+                            {/* <Modal
 
-                        isOpen={isOpen}
-                        onClose={onClose}
-                        size={size} >
-                            <ModalOverlay />
-                            
-                            <ModalContent
-                             //Can't get my dimensions; always compromising elsewhere.
-                                alignItems='center'
-                                textAlign='center'
+                                isOpen={isOpen}
+                                onClose={onClose}
+                                size={size} >
+                                <ModalOverlay />
 
-                                width='50%'
-                                height='48%'
-                                marginTop='8%'
-                                borderRadius='35px'
-                                fontSize='1.6em'
-                               >
-                                <ModalHeader
-                                fontSize='2vw'>Account Verification</ModalHeader>
-                                <ModalCloseButton/>
-                                <ModalBody>
-                                    At Curlo, club accounts matter. We Verify <br></br>
-                                    that every club account that is opened in <br></br>
-                                    connected to a real club that has a curling <br></br>
-                                    location. We ensure that our club admins <br></br>
-                                    are real people that are inputting scores from <br></br>
-                                    real games. <b>This process can take up to a <br></br>
-                                    week.</b> Expect to hear from us during this <br></br>
-                                    verification process as we verify you are <br></br>
-                                    an administrator of the club you are <br></br>
-                                    registered with.
-                                </ModalBody>
+                                <ModalContent
+                                    //Can't get my dimensions; always compromising elsewhere.
+                                    alignItems='center'
+                                    textAlign='center'
 
-                            </ModalContent>
-                        </Modal> 
+                                    width='50%'
+                                    height='48%'
+                                    marginTop='8%'
+                                    borderRadius='35px'
+                                    fontSize='1.6em'
+                                >
+                                    <ModalHeader
+                                        fontSize='2vw'>Account Verification</ModalHeader>
+                                    <ModalCloseButton />
+                                    <ModalBody>
+                                        At Curlo, club accounts matter. We Verify <br></br>
+                                        that every club account that is opened in <br></br>
+                                        connected to a real club that has a curling <br></br>
+                                        location. We ensure that our club admins <br></br>
+                                        are real people that are inputting scores from <br></br>
+                                        real games. <b>This process can take up to a <br></br>
+                                            week.</b> Expect to hear from us during this <br></br>
+                                        verification process as we verify you are <br></br>
+                                        an administrator of the club you are <br></br>
+                                        registered with.
+                                    </ModalBody>
+
+                                </ModalContent>
+                            </Modal> */}
+                        </VStack>
                     </Box>
                 </AuthLayout>
             </Box>
