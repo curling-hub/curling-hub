3fae2c3f509b380bd4f094dc3d61cb73bd5c8752
andresh6 <43798349+andresh6@users.noreply.github.com>
2022-03-14 19:11:20 -0700

    made tests for host signup page (#25)
    Co-authored-by: Andres <herrera8@capstone-dev-0d66.us-west1-b.c.capstone-340900.internal>

---
+++ cypress/integration/host_signup_page.spec.js
@@ -0,0 +1,24 @@
+describe('Host sign up page rendering and navigation', () => {
+    const port = Cypress.env('PORT') || '3000'
+    const baseUrl = `http://localhost:${port}`
+    it('should navigate to the login page', () => {
+        cy.visit(`${baseUrl}/new-host`)
+        cy.get('a').contains('Login').click()
+        cy.url().should('include', '/login')
+    })
+    /*
+    it('should navigate to the request account page', () => {
+        cy.visit(`${baseUrl}/host-signup`)
+        cy.get('button').contains('Request Account').click()
+        cy.url().should('include', '/requestaccount')
+    }) Need a page to direct to when account is requested.*/
+
+    it('should contain a TOS button', () => {
+        cy.visit(`${baseUrl}/new-host`)
+        cy.get('button').contains('Terms of Service').click()
+    })
+    it('should contain a privacy policy button', () => {
+        cy.visit(`${baseUrl}/new-host`)
+        cy.get('button').contains('Privacy Policy').click()
+    })
+})
