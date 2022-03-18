866b3f04c16cd8550607a6f31d786c808442788d
RayChen29 <82558712+RayChen29@users.noreply.github.com>
2022-03-18 15:58:58 -0700

    Request (#23)
    * Added Front+Modal Contents, Modal Sizing issue...
    
    * Adjusted Modal Header Size
    
    * Adjusted line 23 for error check
    
    * Consolidated imports, adjusted box+button sizing
    
    * adjusted spacing and modal component
    
    * adding modal
    
    * Cleaned out comments
    
    Co-authored-by: BennettDesmond <bennett.a.desmond@gmail.com>

---
+++ components/modals/RequestModal.tsx
@@ -0,0 +1,46 @@
+import {
+    Modal,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    Center,
+    Text,
+    OrderedList,
+    ListItem,
+} from '@chakra-ui/react'
+
+interface RequestModalProps {
+    isOpen?: boolean
+    onClose?: () => void
+}
+
+export default function RequestModal(props: RequestModalProps) {
+    const { isOpen = false, onClose = (() => { }) } = props
+    return (
+        <>
+            <Modal isOpen={isOpen} onClose={onClose} scrollBehavior='inside' size='lg'>
+                <ModalContent h="360px" w={{ base: '350px', md: '500px', lg: '650px' }} mt="105px" bg="primary.white" borderRadius="20" shadow="dark-lg">
+                    <ModalHeader>
+                        <Center fontSize='3xl' fontWeight='bold'>
+                            Account Verification
+                        </Center>
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody textAlign="center" pb={6} mx="15px" >
+                        At Curlo, club accounts matter. We Verify
+                        that every club account that is opened in
+                        connected to a real club that has a curling
+                        location. We ensure that our club admins
+                        are real people that are inputting scores from
+                        real games. <b>This process can take up to a
+                            week.</b> Expect to hear from us during this
+                        verification process as we verify you are
+                        an administrator of the club you are
+                        registered with.
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
+    )
+}
--- pages/request.tsx
+++ pages/request.tsx
@@ -1,11 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import AuthLayout from '../components/layouts/AuthLayout'
+import RequestModal from '../components/modals/RequestModal'
 import {
-    Box,
+    Box, Text, Image, Button, Spacer, Center, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, VStack
 } from '@chakra-ui/react'
+import React, { useState } from 'react'
+const linkTextSize = "12"
+
+
 
 const Request: NextPage = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [size, setSize] = useState('sm')
+
+    const handleSizeClick = (newSize: React.SetStateAction<string>) => {//You can ignore the newSize error/warning, for now?
+        setSize(newSize)
+        onOpen()
+    }
+
+    const sizes = ['xl']
     return (
         <>
             <Head>
@@ -19,7 +33,45 @@ const Request: NextPage = () => {
                 bgGradient="linear-gradient(primary.purple, primary.white) repeat"
             >
                 <AuthLayout>
+                    <Box
+                        alignItems='center'
+                        width={{ base: "300px", md: "400px", lg: "400px" }}
+                        height={{ base: "300px", md: "400px", lg: "400px" }}
+                        background="primary.green"
+                        borderRadius="35px"
+                        marginBottom={{ base: '50%', md: '30%', lg: '28%' }}
+                    >
+                        <VStack spacing="3" marginTop="8%">
+                            <Text
+                                fontSize="20px"
+                                textAlign='center'
+                                fontWeight='bold'
+                                padding="6% 6% 0% 6%">
+                                We are processing your <br></br>
+                                request. Expect to hear <br></br>
+                                from us as we verify <br></br>
+                                your account.
+                            </Text>
 
+                            <Image src="/curloLogo2.svg"
+                                alt="Curlo logo"
+                                width={{ base: '70px', md: '100px', lg: '120x' }}
+                                height={{ base: '70px', md: '100px', lg: '120px' }}
+                                marginLeft='40%' />
+                            {/*//Image End */}
+
+                            <Text fontSize={linkTextSize}>
+                                <Button variant="link" size={linkTextSize} onClick={onOpen}>
+                                    What is account verification?
+                                </Button>
+                            </Text>
+                            <RequestModal
+                                isOpen={isOpen}
+                                onClose={onClose}
+                            />
+
+                        </VStack>
+                    </Box>
                 </AuthLayout>
             </Box>
         </>
