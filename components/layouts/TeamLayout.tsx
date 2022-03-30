92e373d9e008937907370b6989a32bd2a11c99be
BennettDesmond <bennett.a.desmond@gmail.com>
2022-03-30 07:22:59 +0000

    Added changes for next sprint i.e. new files and layouts

--- components/layouts/TeamLayout.tsx
+++ components/layouts/TeamLayout.tsx
@@ -1,11 +1,46 @@
-interface TeamLayoutProps {
+import Header from "../navbar/Navbar"
+import { NavItem } from '../navbar/MobileDrawer'
+
+interface LayoutProps {
     children?: React.ReactNode;
 }
 
-export default function TeamLayout({ children }: TeamLayoutProps) {
+export default function TeamLayout({ children }: LayoutProps) {
+    const navItems = getNavItems()
+    const navAnchor = getNavAnchor()
     return (
         <>
-            <main>{ children || <></> }</main>
+            <Header navItems={navItems} navAnchor={navAnchor} />
+            <main>{children || <></>}</main>
         </>
     )
+}
+
+function getNavAnchor(): NavItem {
+    return {
+        label: 'Logout',
+        link: '/',
+    }
+}
+
+function getNavItems(): NavItem[] {
+    const data: NavItem[] = [
+        {
+            label: "My Team",
+            link: '/teams-profile',
+        },
+        {
+            label: "Add Match",
+            link: '/team-add-match',
+        },
+        {
+            label: "Matches",
+            link: '/matches',
+        },
+        {
+            label: "Ratings",
+            link: '/ratings',
+        },
+    ]
+    return data
 }
---
+++ pages/new-team.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import AuthLayout from '../components/layouts/AuthLayout'
+import { Box } from '@chakra-ui/react'
+
+const NewTeam: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>New Team | Curlo</title>
+            </Head>
+            <Box
+                position="absolute"
+                w="100%"
+                h="100vh"
+                bgGradient="linear-gradient(primary.purple, primary.white)"
+            >
+                <AuthLayout />
+            </Box>
+        </>
+    )
+}
+
+export default NewTeam
