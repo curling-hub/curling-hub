8a40d5e99f2a48ad30dff7af9987e6ce0f4ee4b4
Ross Morrison <ros25@pdx.edu>
2022-03-31 19:01:06 +0000

    created frontend for new teams page

---
+++ components/newTeam/newTeamFields.tsx
@@ -0,0 +1,140 @@
+import NextLink from 'next/link'
+import { useState } from 'react'
+import {
+    Link as ChakraLink,
+    Input,
+    Stack,
+    VStack,
+    HStack,
+    Button,
+    Radio,
+    RadioGroup,
+    Select,
+    Text,
+    Checkbox
+} from '@chakra-ui/react'
+
+interface NewTeamFieldsProps {
+    onOpenPrivacyPolicy: () => void;
+    onOpenTermsOfService: () => void;
+}
+
+export default function NewTeamFields(props: NewTeamFieldsProps) {
+    const {
+        onOpenPrivacyPolicy,
+        onOpenTermsOfService
+    } = props
+
+    const [mode, setMode] = useState(true)
+    const [alternate, setAlternate] = useState(false)
+    const [isAgreedPP, setIsAgreedPP] = useState(false)
+    const modeMap = new Map<boolean, string>([
+        [true, "classic"],
+        [false, "doubles"]
+    ])
+
+    return (
+        <VStack alignItems="center" spacing="4">
+            <Input
+                borderRadius="full"
+                focusBorderColor="green.400"
+                shadow="sm"
+                placeholder="Team Name"
+            />
+            <RadioGroup onChange={() => setMode(!mode)} value={modeMap.get(mode)}>
+                <Stack direction="row" alignItems="right">
+                    <Radio colorScheme="green" value="classic">Classic</Radio>
+                    <Radio colorScheme="green" value="doubles">Doubles</Radio>
+                </Stack>
+            </RadioGroup>
+            <Input
+                borderRadius="full"
+                focusBorderColor="green.400"
+                shadow="sm"
+                placeholder="Curler One"
+            />
+            <Input
+                borderRadius="full"
+                focusBorderColor="green.400"
+                shadow="sm"
+                placeholder="Curler Two"
+                />
+            { mode && 
+                <>
+                    <Input
+                        borderRadius="full"
+                        focusBorderColor="green.400"
+                        shadow="sm"
+                        placeholder="Curler Three"
+                    />
+                    <Input
+                        borderRadius="full"
+                        focusBorderColor="green.400"
+                        shadow="sm"
+                        placeholder="Curler Four"
+                    />
+                    {alternate ? 
+                        <>
+                            <Input
+                                borderRadius="full"
+                                focusBorderColor="green.400"
+                                shadow="sm"
+                                placeholder="Alternate"
+                            />
+                            <Button onClick={() => setAlternate(!alternate)} variant="link" color="black" size="xs">
+                                Remove Alternate
+                            </Button>
+                        </>
+                    : 
+                        <Button onClick={() => setAlternate(!alternate)} variant="link" color="black" size="xs">
+                            Add Alternate
+                        </Button>
+                    }
+                </>
+            }
+            <Select
+                // TODO: POPULATE CATEGORIES VIA SERVER SIDE RENDER. SEE JIRA BOARD FOR DETAILS.
+                borderRadius="full"
+                placeholder="Select Categories..."
+            >
+            </Select>
+            <HStack>
+                <Checkbox
+                    aria-label=""
+                    size="sm"
+                    borderRadius="50%"
+                    colorScheme="teal"
+                    checked={isAgreedPP}
+                    onClick={() => setIsAgreedPP(!isAgreedPP)}
+                    css={`
+                        > span:first-of-type {
+                            box-shadow: unset;
+                        }
+                    `}
+                />
+                <Text fontSize="12">
+                {" "}I agree to the {" "}
+                <Button variant="link" size="12" onClick={onOpenTermsOfService}>
+                    Terms of Service
+                </Button>
+                {" "}and{" "}
+                <Button variant="link" size="12" onClick={onOpenPrivacyPolicy}>
+                    Privacy Policy
+                </Button>
+                </Text>
+            </HStack>
+            <Button
+                type='submit'
+                isFullWidth
+                bg="primary.green"
+                borderRadius="full"
+                boxShadow="md"
+                _hover={{ bg: "green.400" }}
+                _active={{ bg: "green.600" }}
+                _focus={{ boxShadow: "lg" }}
+            >
+                Create Account
+            </Button>
+        </VStack>
+    )
+}
--- pages/new-team.tsx
+++ pages/new-team.tsx
@@ -1,9 +1,32 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import AuthLayout from '../components/layouts/AuthLayout'
-import { Box } from '@chakra-ui/react'
+import TermsOfServiceModal from '../components/modals/TermsOfServiceModal'
+import PrivacyPolicyModal from '../components/modals/PrivacyPolicyModal'
+import { 
+    Box, 
+    Container, 
+    Flex, 
+    Image, 
+    useDisclosure,
+    Stack,
+    VStack 
+} from '@chakra-ui/react'
+import NewTeamFields from '../components/newTeam/newTeamFields'
+import { useState } from 'react'
 
 const NewTeam: NextPage = () => {
+    const {
+        isOpen: privacyPolicyIsOpen = false,
+        onOpen: privacyPolicyOnOpen,
+        onClose: privacyPolicyOnClose,
+    } = useDisclosure()
+    const {
+        isOpen: termsOfServiceIsOpen = false,
+        onOpen: termsOfServiceOnOpen,
+        onClose: termsOfServiceOnClose,
+    } = useDisclosure()
+
     return (
         <>
             <Head>
@@ -15,7 +38,42 @@ const NewTeam: NextPage = () => {
                 h="100vh"
                 bgGradient="linear-gradient(primary.purple, primary.white)"
             >
-                <AuthLayout />
+                <AuthLayout>
+                    <Container maxW="2xl" centerContent>
+                        {/* Outer box */}
+                        <Box minW="sm" maxW={{ base: "sm", md: "none" }} w="100%" h="auto" my="4" borderRadius="20" bg="white" shadow="md">
+                            <Flex flexDirection="row" h="100%">
+                                {/* Left */}
+                                <Box
+                                    display="flex"
+                                    alignItems="end"
+                                    justifyContent="center"
+                                    borderRadius="20"
+                                    bg="primary.green"
+                                    h="auto"
+                                    w="100%"
+                                >
+                                     <Image src="/curlo_Logo.svg" w="60%" h="30%" />
+                                </Box>
+                                {/* Sign up container (should only run on client side, e.g mounted) */}
+                                <Box minW="sm" w="100%" h="100%" m={{ base: 0, md: 2 }} p={10} borderRadius="32">
+                                    <PrivacyPolicyModal
+                                        isOpen={privacyPolicyIsOpen}
+                                        onClose={privacyPolicyOnClose}
+                                    />
+                                    <TermsOfServiceModal
+                                        isOpen={termsOfServiceIsOpen}
+                                        onClose={termsOfServiceOnClose}
+                                    />
+                                    <NewTeamFields
+                                        onOpenPrivacyPolicy={() => { privacyPolicyOnOpen() }}
+                                        onOpenTermsOfService={() => { termsOfServiceOnOpen() }}
+                                    />
+                                </Box>
+                            </Flex>
+                        </Box>
+                    </Container>
+                </AuthLayout>
             </Box>
         </>
     )
