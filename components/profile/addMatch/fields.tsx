68bbdbf957f7ef6bfb7db7f5cd532e0d6382a6cd
Vincent Vermilya <TapeWerm@users.noreply.github.com>
2022-04-06 09:52:10 +0000

    Remove hidden margin

--- components/profile/addMatch/fields.tsx
+++ components/profile/addMatch/fields.tsx
@@ -102,7 +102,7 @@ const Fields = (props: FieldsProps): JSX.Element => {
                                             <FormControl>
                                                 <Flex justifyContent="space-between" {...group}>
                                                     {resultOptions.map((value) => (
-                                                        <FormLabel htmlFor={value.toLowerCase()} m='auto' srOnly>{value}</FormLabel>
+                                                        <FormLabel htmlFor={value.toLowerCase()} srOnly>{value}</FormLabel>
                                                     ))}
                                                     {resultOptions.map((value) => {
                                                         const radioProps = getRadioProps({value});
