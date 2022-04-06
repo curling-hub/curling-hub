26c9ecc3ed61ded8ef078f8de58cb62351f0f096
kotoriのねこ <2567702743@qq.com>
2022-04-06 07:40:29 +0000

    Implement host information API and corresponding db queries

--- lib/handlers/hosts.ts
+++ lib/handlers/hosts.ts
@@ -1,20 +1,42 @@
 import { RowDataPacket } from 'mysql2'
 import { pool } from '../db'
+import { HostInfo } from '../models'
 
 
 export async function hosts() {
     return 0
 }
 
-export async function getAllHosts() {
+export async function getAllHosts(): Promise<HostInfo[]> {
     // TODO: Find out how hosts are stored in the db
-    const query = `SELECT NULL AS team_id, NULL as name, NULL as rating;`
+    const query = `
+        SELECT
+            host_id,
+            organization,
+            website,
+            phone_number,
+            street_address,
+            address_extras,
+            city,
+            state,
+            zip,
+            country,
+            updated_at
+        FROM host_profile;
+    `
     const [rows, _] = await pool.promise().query(query)
     const r = rows as RowDataPacket[]
     return r.map((val) => ({
-        teamId: val['team_id'],
-        name: val['name'],
-        rating: val['rating'],
+        hostId: val['host_id'],
+        organization: val['organization'],
+        website: val['website'],
+        phoneNumber: val['phone_number'],
+        streetAddress: val['street_address'],
+        addressExtras: val['address_extras'],
+        city: val['city'],
+        state: val['state'],
+        zip: val['zip'],
+        country: val['country'],
     }))
 }
 
@@ -34,7 +56,45 @@ export async function getIceSheetsByHostId(hostId: string) {
     }))
 }
 
-export async function getHostInfoById(hostId: string) {
+export async function getHostInfoById(hostId: string): Promise<HostInfo | null> {
     // TODO: implement host info query including the ice sheets info
-    return {}
+    const query = `
+        SELECT
+            host_profile.host_id,
+            host_profile.organization,
+            host_profile.website,
+            host_profile.phone_number,
+            host_profile.street_address,
+            host_profile.address_extras,
+            host_profile.city,
+            host_profile.state,
+            host_profile.zip,
+            host_profile.country,
+            host_profile.updated_at,
+            JSON_ARRAYAGG(ice_sheets.name) AS ice_sheets
+        FROM host_profile
+        INNER JOIN ice_sheets
+            ON ice_sheets.host_id = host_profile.host_id
+        WHERE host_profile.host_id = ?
+        GROUP BY host_profile.host_id;
+    `
+    const [rows, _] = await pool.promise().query(query, [hostId])
+    const r = rows as RowDataPacket[]
+    if (r.length === 0) {
+        return null
+    }
+    const val = r[0]
+    return Object.assign({}, {
+        hostId: val['host_id'],
+        organization: val['organization'],
+        website: val['website'],
+        phoneNumber: val['phone_number'],
+        streetAddress: val['street_address'],
+        addressExtras: val['address_extras'],
+        city: val['city'],
+        state: val['state'],
+        zip: val['zip'],
+        country: val['country'],
+        iceSheets: val['ice_sheets'].filter((val: string | null) => !!val),
+    })
 }
--- lib/models/host.ts
+++ lib/models/host.ts
@@ -1,4 +1,14 @@
 export interface HostInfo {
     hostId: string
-    name: string
+    organization: string
+    website?: string
+    phoneNumber: string
+    streetAddress: string
+    addressExtras?: string
+    city: string
+    state: string
+    zip: string
+    country: string
+    updatedAt?: Date
+    iceSheets?: string[]
 }
