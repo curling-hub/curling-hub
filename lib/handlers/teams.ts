84ec4a16291ee735d542769b33a1afb66a9d9a59
ausbrown <59979994+ausbrown@users.noreply.github.com>
2022-04-11 20:23:52 -0700

    Update teams.ts
    removed duplicate of `getAllTeams()`

--- lib/handlers/teams.ts
+++ lib/handlers/teams.ts
@@ -10,23 +10,6 @@ export async function teams() {
     return 0
 }
 
-export async function getAllTeams() {
-    const query = `
-        SELECT
-            team_id,
-            name,
-            rating
-        FROM team_profile
-    `
-    const [rows, _] = await pool.promise().query(query)
-    const r = rows as RowDataPacket[]
-    return r.map((val) => ({
-        teamId: val['team_id'],
-        name: val['name'],
-        rating: val['rating'],
-    }))
-}
-
 export async function getTeamInfo(team_Id: string) {
     const query = `
         SELECT DISTINCT
