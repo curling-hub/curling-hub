994d7550f316e8f2aa6c3b81a31c43640e163e4c
Ross Morrison <ros25@pdx.edu>
2022-04-12 18:16:00 +0000

    started ui

--- pages/ratings.tsx
+++ pages/ratings.tsx
@@ -1,16 +1,75 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
 import Footer from "../components/footer/footer";
+import { getSession } from 'next-auth/react'
+import { Session } from 'next-auth'
+import TeamLayout from '../components/layouts/TeamLayout'
+import StandardLayout from '../components/layouts/StandardLayout'
+import {
+    Box
+} from '@chakra-ui/react'
+import RatingsBox from '../components/ratings/ratingsBox'
 
-const Ratings: NextPage = () => {
+
+interface RatingsProps {
+    user?: Session
+}
+
+const Ratings: NextPage<RatingsProps> = (props: RatingsProps) => {
+    
     return (
         <>
             <Head>
                 <title>Ratings | curlo</title>
             </Head>
-            <h1> Ratings </h1>
+            <Box
+                position="absolute"
+                w="100%"
+                h="100vh"
+                bgGradient="linear-gradient(primary.purple, primary.white)"
+            >
+            {
+                props.user ? 
+                    <TeamLayout>
+                        <RatingsBox/>
+                    </TeamLayout>
+                :
+                    <StandardLayout>
+                        <RatingsBox/>
+                    </StandardLayout>
+            }
+            </Box>
         </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const session = await getSession(context)
+    if (!session || !session["user"]) {
+        // not signed in / signed up
+        return {
+            props: {
+                user: null
+            }
+        }
+    }
+
+    const user = session["user"]
+    if (!user["account_type"]) {
+        // has not completed sign up up
+        return {
+            props: {
+                user: null
+            },
+        }
+    }
+
+    // signed in, share session with component
+    return {
+        props: {
+            user: session
+        },
+    }
 }
 
 export default Ratings
