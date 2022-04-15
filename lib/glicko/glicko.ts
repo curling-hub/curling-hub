afdfe65f5f43ef09e819d731e27c6f8d85827c5e
Vincent Vermilya <TapeWerm@users.noreply.github.com>
2022-04-15 11:43:14 +0000

    Add query
    Still very incomplete

--- lib/glicko/glicko.ts
+++ lib/glicko/glicko.ts
@@ -1,9 +1,18 @@
-import mysql from 'mysql2';
-import glicko from 'glicko2';
+import { pool } from '../db';
+
+var glicko2 = require('glicko2');
 
 // Use glicko2 to update team ratings
 
 // TODO: Use sequelize
-// SELECT team_profile.team_id, team_glicko_info.rating, team_glicko_info.rating_deviation, team_glicko_info.volatility
-// FROM team_profile
-// JOIN team_glicko_info ON team_profile.team_id = team_glicko_info.team_id;
+const query_teams = `
+    SELECT
+        team_profile.team_id,
+        team_glicko_info.rating,
+        team_glicko_info.rating_deviation,
+        team_glicko_info.volatility
+    FROM team_profile
+    JOIN team_glicko_info
+    ON team_profile.team_id = team_glicko_info.team_id
+`
+const rows = pool.query(query_teams);
