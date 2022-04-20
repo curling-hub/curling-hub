d610fe978bf4d13eac627ef764d1c2c687c50252
Ross Morrison <ros25@pdx.edu>
2022-04-20 08:05:01 +0000

    fixed merge error

--- lib/handlers/matches.ts
+++ lib/handlers/matches.ts
@@ -14,7 +14,6 @@ export async function create(form: AddMatchSchema): Promise<MatchResult> {
     } else if (form.matchResult.toUpperCase() === 'LOSS') {
         winner = 'team_id_2'
     }
-<<<<<<< HEAD
     const result = await sequelize.transaction(async (t) => {
         // 1. Create match result
         const matchResult = await DbModels.MatchModel.create({
@@ -32,58 +31,5 @@ export async function create(form: AddMatchSchema): Promise<MatchResult> {
         return matchResult.get()
     })
     return result
-=======
-    const queryInsertMatch = `
-        INSERT INTO match_info (
-            host_id,
-            team_id_1,
-            team_id_2,
-            winner,
-            sheet_of_ice,
-            comments,
-            category_id,
-            date
-        ) VALUES (
-            ?, ?, ?, ?, ?, ?, ?, NOW()
-        );
-    `
-    const queryInsertMatchArgs = [
-        form.location,
-        form.currentTeam,
-        form.opponent,
-        winner,
-        form.sheetOfIce,
-        form.comments,
-        form.category,
-        form.date,
-    ]
-    const queryInsertMatchTeamRel = `
-        INSERT INTO match_team_rel (
-            team_id, match_id
-        ) VALUES (
-            ?, ?
-        ) ON DUPLICATE KEY UPDATE
-            team_id=team_id,
-            match_id=match_id;
-    `
-    const conn = await pool.promise().getConnection()
-    try {
-        await conn.beginTransaction()
-        // 1. Insert match
-        const [results, _] = await conn.query(queryInsertMatch, queryInsertMatchArgs)
-        const r = results as OkPacket
-        const matchId = r.insertId
-        // 2. Insert match team rel
-        await Promise.all([
-            conn.query(queryInsertMatchTeamRel, [form.opponent, matchId]),
-            conn.query(queryInsertMatchTeamRel, [form.currentTeam, matchId]),
-        ])
-        await conn.commit()
-    } catch (error: any) {
-        await conn.rollback()
-    } finally {
-        conn.release()
-    }
->>>>>>> initial db query
 }
 
