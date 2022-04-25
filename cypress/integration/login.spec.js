1413890765468e49e2abb293ea5821a01ffc0474
Andres <herrera8@capstone-dev-0d66.us-west1-b.c.capstone-340900.internal>
2022-04-25 02:22:27 +0000

    updated test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -4,7 +4,7 @@ describe('Login page rendering and navigation', () => {
     it('should navigate to the signup page', () => {
         cy.visit(`${baseUrl}/login`)
         cy.get('a').contains('Sign up').click()
-        cy.url().should('include', '/newuser')
+        cy.url().should('include', '/signup')
     })
     it('should contain a Login with Email button', () => {
         cy.visit(`${baseUrl}/login`)
