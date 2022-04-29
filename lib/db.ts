99400cc4576b6bb3599b296c48ceaf2d75473c1c
kotoriのねこ <2567702743@qq.com>
2022-04-29 03:53:02 +0000

    Refactor `/api/health` endpoint for simple API test

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import mysql, { OkPacket, RowDataPacket } from 'mysql2'
+import mysql from 'mysql2'
 import { Sequelize } from 'sequelize'
 import getConfig from 'next/config'
 
@@ -13,7 +13,8 @@ const {
 
 export function getSequelize() {
     if (process.env.NODE_ENV === 'test') {
-        return new Sequelize('sqlite::memory:')
+        const db = new Sequelize('sqlite::memory:')
+        return db
     }
     return new Sequelize(mysql_database, mysql_user, mysql_password, {
         host: mysql_host,
@@ -26,6 +27,7 @@ export function getSequelize() {
 // Use sequelize to avoid formatting between db rows and js objects
 export const sequelize = getSequelize()
 
+// Deprecated in favor of sequelize
 export const pool = mysql.createPool({
     host: mysql_host,
     port: mysql_port,
@@ -33,13 +35,3 @@ export const pool = mysql.createPool({
     password: mysql_password,
     database: mysql_database
 })
-
-export async function health(): Promise<Number> {
-    const query = `SELECT 1 AS status`
-    const [rows, _] = await pool.promise().query(query)
-    const r = rows as RowDataPacket[]
-    if (r.length === 0) {
-        return 0
-    }
-    return r[0]['status'] as Number
-}
