e8420644d3f6c16c80c8d2509559c032beb1df9c
kotoriのねこ <2567702743@qq.com>
2022-05-04 20:45:06 +0000

    Remove duplicated interfaces

--- lib/models/match.ts
+++ lib/models/match.ts
@@ -24,10 +24,7 @@ export interface MatchResult {
 export interface MatchResultDetails extends MatchResult {
     teams: TeamInfo[],
 }
-export interface Filter {
-    filter_id: number
-    value: string
-}
+
 export interface Filter {
     filter_id: number
     value: string
