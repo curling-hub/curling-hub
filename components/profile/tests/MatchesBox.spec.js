1ee0c6faa46793236632663c1c33fae35df0f51a
ausbrown <ausjbrown@gmail.com>
2022-05-09 18:12:37 +0000

    added tests for MatchesBox and MembersTable

---
+++ components/profile/tests/MatchesBox.spec.js
@@ -0,0 +1,67 @@
+import * as React from 'react'
+import { mount } from '@cypress/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { TeamMatch } from '../../../lib/models/teams'
+import { MatchesBox, MatchesTable, ProfileButton, SideBySideContainer } from '../'
+
+/** @type {TeamMatch} */
+const matches = [
+    {
+        date: '2022-04-30',
+        matchId: 1,
+        teamId1: 1,
+        teamId2: 2,
+        winner: 'team_id_1',
+        hostId: 'id',
+        host: {
+            hostId: 'id',
+            organization: 'Husky',
+            website: 'http://example.com',
+            phoneNumber: '(555) 555-5555',
+            streetAddress: '',
+            city: '',
+            state: '',
+            zip: '',
+            country: '',
+            status: 'accepted',
+        },
+        sheetOfIce: 'Sheet A',
+        comments: 'wet ice',
+        teams: [
+            {
+                teamId: 1,
+                name: 'Team 1',
+                rating: '',
+            },
+            {
+                teamId: 2,
+                name: 'Team 2',
+                rating: '',
+            },
+        ]
+    }
+]
+
+it('Team Profile Matches Table', () => {
+    mount(
+        <ChakraProvider>
+            <SideBySideContainer>
+                <MatchesBox
+                    color="primary.white"
+                    boxShadow='lg'
+                >
+                    <Text fontSize="2.5rem" marginTop="5px" fontWeight="bold">
+                        Matches
+                    </Text>
+                    <MatchesTable teamMatches={matches} teamId={matches.teamId1} />
+                    <Box marginTop="63px">
+                        <Link href="/matches">
+                            <ProfileButton buttonText='View Matches' color='primary.green' top="-20px" />
+                        </Link>
+                    </Box>
+                </MatchesBox>
+            </SideBySideContainer>
+        </ChakraProvider>
+    )
+    cy.get('button').contains('View Matches').click()
+})
---
+++ components/profile/tests/MembersTable.spec.js
@@ -0,0 +1,129 @@
+import * as React from 'react'
+import { mount } from '@cypress/react'
+import { ChakraProvider, VStack } from '@chakra-ui/react'
+import { TeamWithMembersAndRatings } from '../../../lib/models/teams'
+import { Category, TeamMember } from '../../../lib/models'
+import { ProfileButton, SideBySideContainer, LeftHandBox } from '../'
+
+/** @type {TeamWithMembersAndRatings} */
+const teamInfo =
+{
+    teamId: 1,
+    name: "Test Team Name",
+    rating: "9001",
+    members: ["Team_Member_1", "Team_Member_2", "Team_Member_3", "Team_Member_4"],
+    teamGlickoInfo: {
+        teamId: 1,
+        rating: 9001,
+        ratingDeviation: 264.91,
+        volatility: 1,
+    },
+    ratingPeriods: {
+        ratingPeriodId: 1,
+        name: "Test Rating Period",
+        startDate: '2022-04-30',
+        endDate: '2022-05-10',
+        teamGlickoInfo: {
+            teamId: 1,
+            rating: 9001,
+            ratingDeviation: 264.91,
+            volatility: 1,
+        }
+    }
+}
+
+
+/** @type {Category[]} */
+const teamCategory = [
+    {
+        categoryId: "Id_1",
+        name: "First Category",
+    },
+    {
+        categoryId: "Id_2",
+        name: "Second Category",
+    }
+]
+
+/** @type {TeamMember[]} */
+const members = [
+    {
+        memberId: 1,
+        teamId: 1,
+        name: "Team_Member_1",
+        email: "Team_Member_1@test.mail"
+    },
+    {
+        memberId: 2,
+        teamId: 1,
+        name: "Team_Member_2",
+        email: "Team_Member_2@test.mail"
+    },
+    {
+        memberId: 3,
+        teamId: 1,
+        name: "Team_Member_3",
+        email: "Team_Member_3@test.mail"
+    },
+    {
+        memberId: 4,
+        teamId: 1,
+        name: "Team_Member_4",
+        email: "Team_Member_4@test.mail"
+    },
+]
+
+const email = "Test_Team@test.mail"
+
+it('Team Profile Members Table', () => {
+    mount(
+        <ChakraProvider>
+            <SideBySideContainer height='1022px'>
+                <VStack spacing="78px" height="100%">
+                    <LeftHandBox color='primary.white'>
+                        <VStack spacing="0px" h="356px">
+                            {teamInfo && (
+                                <Text
+                                    fontSize="2.5em"
+                                    fontWeight="bold"
+                                >
+                                    {teamInfo.name}
+                                </Text>
+                            )}
+                            <Text
+                                fontSize="1.5em"
+                                fontWeight="bold"
+                            >
+                                Contact
+                            </Text>
+                            <Text>
+                                {email}
+                            </Text>
+                            <MembersTable teamMembers={members} teamCategories={teamCategory} />
+                        </VStack>
+                        <ProfileButton buttonText='Edit' color='primary.gray' />
+                    </LeftHandBox>
+                    <LeftHandBox color='primary.green'>
+                        <VStack>
+                            <Text
+                                fontSize="2.5em"
+                                fontWeight="bold"
+                            >
+                                Rating
+                            </Text>
+                            {teamInfo && (
+                                <Text
+                                    fontSize="6em"
+                                    fontWeight="bold"
+                                >
+                                    {teamInfo.teamGlickoInfo?.rating}
+                                </Text>
+                            )}
+                        </VStack>
+                    </LeftHandBox>
+                </VStack>
+            </SideBySideContainer>
+        </ChakraProvider>
+    )
+    cy.get('button').contains('Edit').click()
+})
