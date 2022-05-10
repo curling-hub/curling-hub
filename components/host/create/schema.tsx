81094b3b0dd908b6a39fcf7f4c190dd25a21f3fe
Vincent Vermilya <TapeWerm@users.noreply.github.com>
2022-05-09 22:22:18 -0700

    Ch 74 new host (#63)
    * Accessibility
    
    Move ToS
    Remove login button
    Still gotta redo phone
    
    * Replace phoneType with countryCode
    
    * Use new link styling
    
    * Remove countryCode from phoneRE
    
    Prepend countryCode in API
    
    * Swap phone field order

--- components/host/create/schema.tsx
+++ components/host/create/schema.tsx
@@ -1,13 +1,13 @@
 import * as yup from 'yup'
 
-const phoneRE = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/g;
+const phoneRE = /^(?:(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/g;
 const zipRE = /^\d{5}(?:[- ]?\d{4})?$/
 
 const hostSignupSchema = yup.object({
     organization: yup.string().required(),
     website: yup.string().required().url().nullable(),
     phone: yup.string().matches(phoneRE, 'Invalid phone number').required(),
-    phoneType: yup.string().required("Phone type is a required field"),
+    countryCode: yup.string().required(),
     address: yup.string().required(),
     address2: yup.string().optional(),
     city: yup.string().required(),
