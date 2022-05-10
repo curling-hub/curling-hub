6ba572c84d5b9c524f1ab4975ca0f8f0a04310b5
RayChen29 <82558712+RayChen29@users.noreply.github.com>
2022-05-10 14:07:10 -0700

    Added filter, then mapped the filtered options.

--- components/profile/addMatch/fields.tsx
+++ components/profile/addMatch/fields.tsx
@@ -31,6 +31,9 @@ import schema from './schema'
 import ResultRadio from './resultRadio'
 import type { HostInfo, TeamInfo } from '../../../lib/models'
 
+import { RowDataPacket } from 'mysql2'
+import { t } from '@chakra-ui/styled-system/dist/declarations/src/utils'
+
 
 const getInitialValues = (otherFields: any = {}) => ({
     matchResult: 'Win',
@@ -99,10 +102,17 @@ const Fields = (props: FieldsProps): JSX.Element => {
     })
     const group = getRootProps()
 
-    const teamOptions = teams.map((t) => ({
+    const filteredTeamOptions = teams.filter((team) => team.teamId != currentTeam.teamId);//Not sure if using right fields / can't really use the page.
+
+    const teamOptions = filteredTeamOptions.map((team) => {
+        return {value: team.teamId,label: team.name};
+    })
+
+    {/* old
+    const teamOptions = teams.map((t) => ({ 
         value: t.teamId,
         label: t.name,
-    }))
+    }))*/}
     const hostOptions = hosts.map((h) => ({
         value: h.hostId,
         label: h.organization,
