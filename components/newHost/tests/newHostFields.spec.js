cabcac82038209b80edc19090203f677653c4311
kotoriのねこ <2567702743@qq.com>
2022-05-12 21:59:33 +0000

    Update tests to reflect field id change

--- components/newHost/tests/newHostFields.spec.js
+++ components/newHost/tests/newHostFields.spec.js
@@ -23,7 +23,7 @@ it('New Host Page', () => {
     cy.get('#state').select('New York')
     cy.get('#zip').type('55555')
     cy.get('#country').select('USA')
-    cy.get('#iceSheets').type('5{enter}')
+    cy.get('#iceSheetCount').type('5{enter}')
     cy.get('input[type="checkbox"]').check({force: true})
     cy.get('button').contains('Request Account').click().then(() => {
         expect(clicked).to.be.true
