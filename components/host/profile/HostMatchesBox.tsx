c53cafc767886a59bc09563657433d4c5e67afbb
ausbrown <ausjbrown@gmail.com>
2022-05-16 18:47:17 +0000

    initial push

--- components/host/profile/HostMatchesBox.tsx
+++ components/host/profile/HostMatchesBox.tsx
@@ -18,45 +18,48 @@ export default function HostInfoBox(props: HostInfoBoxProps) {
 
     return (
         <>
-            <Flex
-                align="center"
-                minW="110px"
-                direction="column"
-                h="60%"
-                marginTop="31px"
-            >
-                <Text
-                    fontSize="2.5em"
-                    fontWeight="bold"
-                    h="120px"
+            <Box h="75%">
+                <Flex
+                    align="center"
+                    minW="110px"
+                    direction="column"
+                    h="100%"
+                    marginTop="31px"
                 >
-                    Recently Added Matches
-                </Text>
-                <TableContainer w="75%" maxW="388px" minW="302px" overflowY="auto">
+                    <Text
+                        fontSize="2.5em"
+                        fontWeight="bold"
+                        h="120px"
+                    >
+                        Recently Added Matches
+                    </Text>
+                    <TableContainer w="90%" /* maxW="388px" */ minW="302px" overflowY="auto">
                         <Table variant='simple' size="sm" whiteSpace="normal">
-                        <Thead>
-                            <Tr>
-                                <Td fontWeight="bold">Date</Td>
-                                <Td fontWeight="bold">Team One</Td>
-                                <Td fontWeight="bold">Team Two</Td>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {hostMatches.map((match) => {
-                                return <Tr key={`${match.matchId}`}>
-                                    <td>{match.date}</td>
-                                    <td>{match.team1}</td>
-                                    <td>{match.team2}</td>
-                                    <td>
-                                        <FormLabel id={`edit-${match.matchId}`} srOnly>Edit</FormLabel>
-                                        <GrFormEdit aria-labelledby={`edit-${match.matchId}`} />
-                                    </td>
+                            <Thead>
+                                <Tr>
+                                    <Td fontWeight="bold">Date</Td>
+                                    <Td fontWeight="bold">Team One</Td>
+                                    <Td fontWeight="bold">Team Two</Td>
+                                    <Td>{/* Placeholder */}</Td>
                                 </Tr>
-                            })}
-                        </Tbody>
-                    </Table>
-                </TableContainer>
-            </Flex>
+                            </Thead>
+                            <Tbody>
+                                {hostMatches.map((match) => (
+                                    <Tr key={`${match.matchId}`}>
+                                        <Td>{match.date}</Td>
+                                        <Td>{match.team1}</Td>
+                                        <Td>{match.team2}</Td>
+                                        <Td>
+                                            <FormLabel id={`edit-${match.matchId}`} srOnly>Edit</FormLabel>
+                                            <GrFormEdit aria-labelledby={`edit-${match.matchId}`} />
+                                        </Td>
+                                    </Tr>
+                                ))}
+                            </Tbody>
+                        </Table>
+                    </TableContainer>
+                </Flex>
+            </Box>
         </>
     );
 }
